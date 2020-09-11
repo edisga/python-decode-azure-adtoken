decb07ecf5c6e352f97f94fdb5bcf4d05e956d7b
Edison Garcia Chinas <edisga@microsoft.com>
2020-09-10 19:37:05 -0500

    Updating

--- index.py
+++ index.py
@@ -6,20 +6,20 @@ import jwt
 
 def decode_token(token):
   decoded = jwt.decode(token, verify=False)
-
   for key in decoded.keys():
      print(key + ': ' + str(decoded[key]))
+  return decoded
 
 @app.route("/")
 def home():
 
-    token1 =  request.headers.get('x-ms-token-aad-id-token')
-    token2 =  request.headers.get('x-ms-token-aad-access-token')
+    token =  request.headers.get('x-ms-token-aad-id-token')
+    #token2 =  request.headers.get('x-ms-token-aad-access-token')
 
-    print(token1)
+    print(token)
     print("--------------")
-    print(token2)
-    token_decoded = decode_token(token1)
+    #print(token2)
+    token_decoded = decode_token(token)
     return token_decoded
 
 if __name__ == '__main__':
